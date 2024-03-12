d4519131535359768557e9ff97cea7a6e0aa80f7
Abhinay Pratap <abhinaypratap@icloud.com>
2024-03-12 23:18:09 +0530

    set up project

---
+++ Parameter/Application/SceneDelegate.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        let navigationController = UINavigationController(rootViewController: HomeViewController())
+        navigationController.navigationBar.prefersLargeTitles = true
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
+}
---
+++ README.md
@@ -0,0 +1,35 @@
+<img src="https://github.com/abhinaypratap/017-uikit-declarative-ui/blob/main/Resources/AppIcon-1024-round-corner.png" align="right" width="120" height="120"/>
+
+# Parameter
+
+## Project Metadata
+
+- ✅ SwiftLint
+- ❌ Unit Tests
+- ❌ UI Tests
+- [Project](https://github.com/abhinaypratap/017-uikit-declarative-ui)
+- Frameworks
+    - Foundation
+    - UIKit
+- Target: iOS 16.0+
+- Supported Destinations: iPhone
+- Development Team: `Info.config`
+- Project Bundle Identifier: `Info.config`
+- Source Control: Git
+- Dependencies:
+    - 
+---
+
+## Description
+
+---
+
+## Key Concepts
+
+---
+
+## References
+
+- [Paul Hudson | YouTube](https://www.youtube.com/live/Fmu6DlKfRhc?si=kLiSCOg_m6H16DBi)
+
+---
